2e8fdfcfd156508d08f2432098afc084464ad7b7
Sergiu Voicu <sergiu.ciprian.voicu@gmail.com>
2021-07-07 18:39:11 +0300

    Third commit

--- src/db/cursurul2/ArrayTest.java
+++ src/db/cursurul2/ArrayTest.java
@@ -2,18 +2,25 @@ package db.cursurul2;
 
 public class ArrayTest {
 
+    public static void printSpecialName(String name){
+        if(name.length() > 4){
+            System.out.println(name);
+        }
+    }
+   public static void printSpecialName(String name, int size){
+        if(name.length() > size){
+            System.out.println(name);
+        }
+    }
+
     public static void main(String[] args) {
 
         for (String s : args) {
-            if (s.length() > 4) {
-                System.out.println(s);
-            }
+            printSpecialName(s);
         }
 
         for (int i = 0; i < args.length; i++) {
-            if (args[i].length() > 4) {
-                System.out.println(args[i]);
-            }
+            printSpecialName(args[i],2);
         }
     }
 }
