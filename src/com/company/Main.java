e914e5ba71aa4add127441a57d9aa7d2f8b89869
cristian.iliescu <ciliescu@veridiumid.com>
2021-07-07 19:21:46 +0300

    references to objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,18 @@ public class Main {
 
 
         System.out.println("Hello, World!");
+
+        String immutableString = "abc";
+        test(immutableString);
+        System.out.println("immutableString:" + immutableString);
+        immutableString = "qwe";
+        System.out.println("immutableString:" + immutableString);
+    }
+
+    public static void test(String s) {
+        String ref = s;
+        s = "xyz";
+        System.out.println("ref:" + ref);
+        System.out.println("s:" + s);
     }
 }
