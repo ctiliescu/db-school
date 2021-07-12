fb580dabae6a61bde3f48e4c7b60933dd24b80cc
Bogdan <bbancu2000@yahoo.com>
2021-07-12 18:35:05 +0300

    curs4-tip si subtip generic +subclase

--- src/db/cursul4/MyFirstWildcard.java
+++ src/db/cursul4/MyFirstWildcard.java
@@ -7,13 +7,17 @@ public class MyFirstWildcard {
 
     public static void main(String[] args) {
         List<Automobile> myList = new ArrayList<Automobile>();
+
+        Automobile car1 = new Automobile.Dacia();
+        Automobile car2 = new Automobile.BMW();
+
         myList.add(new Automobile.Dacia());
         myList.add(new Automobile.BMW());
         myList.add(new Automobile());
         listAutomobiles(myList);
     }
 
-    public static void listAutomobiles(List<? extends Automobile> myList) {
+    public static void listAutomobiles(List<Automobile> myList) {
         for(Automobile automobile : myList) {
             System.out.println(automobile.getType());
         }
